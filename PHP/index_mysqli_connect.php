9bdcd76ef0637de0e11523633a4db9700cb10821
chiachun wu <a0283137@gmail.com>
2022-05-27 15:47:13 +0800

    05_27 PHP跟MySQL 結合練習

---
+++ PHP/index_mysqli_connect.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP連線資料庫</title>
+    <style>
+        h1, h2, h3, h4{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>php連線資料庫</h1>
+        <?php
+            // $dsn="mysql:host=localhost;charset=utf8;dbname=school2";
+            // $pdo=new PDO($dsn , 'root' , '');
+            $conn=mysqli_connect('localhost' , 'root' , '' , 'school2');
+
+            $sql= "SELECT `students`.* , `dept`.`code` , `dept`.`name` as `科系` FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
+
+            // $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_BOTH);
+            $query=mysqli_query($conn,$sql);
+
+
+            //一次看全部
+            $rows=mysqli_fetch_all($query,MYSQLI_BOTH);
+            
+
+            //一次只看一筆
+            // $rows=mysqli_fetch_array($query,MYSQLI_BOTH);
+
+
+            echo "<pre>";
+            print_r($rows);
+            echo "</pre>";
+
+            
+
+
+        ?>
+
+</body>
+</html>
