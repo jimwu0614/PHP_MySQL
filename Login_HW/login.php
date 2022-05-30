b76be3462bff7f9644367148018a1c3170442b24
chiachun wu <a0283137@gmail.com>
2022-05-30 15:51:01 +0800

    會員登入系統作業_完成雛形

---
+++ Login_HW/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登入</title>
+</head>
+<body>
+    <form action="chk_login" method="POST">
+        <table>
+            <tr>
+                <td>帳號</td>
+                <td><input type="text" name="" id=""></td>
+            </tr>
+            <tr>
+                <td>密碼</td>
+                <td><input type="password" name="" id=""></td>
+            </tr>
+            <tr>
+                <td><a href="">註冊</a></td>
+                <td><a href="">忘記</a></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
