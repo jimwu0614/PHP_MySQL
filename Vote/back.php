4beb83a9c75d1f2ea94bd9eea2259a3debadc7c4
chiachun wu <a0283137@gmail.com>
2022-06-13 14:48:39 +0800

    增加對include 檔案的判斷

--- Vote/back.php
+++ Vote/back.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VOTE-manegement</title>
+    <title>Voting_Menegement</title>
+    <link rel="stylesheet" href="./css/back.css">
+    <link rel="stylesheet" href="./css/basic.css">
 </head>
 <body>
+<div id="header">
+    <?php include "./layout/header.php";
+          include "./layout/nav_back.php";
+    ?>
+</div>
+<div id="container">
+    <?php
+    if (isset($_GET['do'])) {
+        $file= "./back/".$_GET['do'].".php";
+    }
+    if (isset($file) && file_exists($file)) {
+        include $file;
     
+    } else {
+    ?>
+    <button class="btn btn-primary" onclick="location.href='?do=add_vote'">新增投票</button>
+    
+    <div>投票列表</div>
+<?php
+}
+?>
+</div>
+
+
+
+<div>
+    <?php include "./layout/footer.php";?>
+</div> 
 </body>
 </html>
--- Vote/index.php
+++ Vote/index.php
@@ -5,8 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online-Voting-HW</title>
+    <link rel="stylesheet" href="./css/basic.css">
+    <link rel="stylesheet" href="./css/front.css">
 </head>
 <body>
-    
+
+<div id="header">
+    <?php include "./layout/header.php";
+          include "./layout/nav_front.php";
+    ?>
+
+</div>
+<div id="container">
+
+</div>
+<div>
+    <?php include "./layout/footer.php";?>
+</div>    
 </body>
 </html>
