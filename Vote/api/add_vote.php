7ed047ac1102ef41f60b374b87d796ef86c1fcb4
chiachun wu <a0283137@gmail.com>
2022-06-14 01:11:03 +0800

    function

---
+++ Vote/api/add_vote.php
@@ -0,0 +1,10 @@
+<?php
+include_once "base.php";
+
+$subject=$_POST['subject'];
+
+
+
+
+header("location:../back.php")
+?>
--- Vote/back.php
+++ Vote/back.php
@@ -1,3 +1,6 @@
+<?php
+include_once ".api/base.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Voting_Menegement</title>
-    <link rel="stylesheet" href="./css/back.css">
     <link rel="stylesheet" href="./css/basic.css">
+    <link rel="stylesheet" href="./css/back.css">
 </head>
 <body>
 <div id="header">
